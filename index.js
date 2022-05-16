14296dca2fd703d0eb4708527029ccf9de49de49
PNourian <nourian.parsa@gmail.com>
2022-05-16 18:03:34 +0430

    base site

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http')
+var fs = require('fs')
+var data = require('./data.json')
+
+var server = http.createServer(serverHandler)
+    .listen(80);
+
+function serverHandler(req, res) {
+    res.statusCode = 200
+
+    if (req.method === 'GET' && (req.url == "/" || req.url == "/home.html")) {
+        
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(path.resolve(__dirname, 'index.html')).pipe(res);
+    }
+
+    if (req.method === 'GET' && req.url == "/data/Ownership") {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data.OwnershipTransfer))
+    }
+
+    if (req.method === 'GET' && req.url == "/data/Whitelist") {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data.Whitelist))
+    }
+}
